520d656ae675b1aff64b7baaa8da9466b75db44a
Mitchting <amitchell@ctatax.uk.com>
2023-08-17 16:41:43 +0100

    Add JSON to CSV conversion functions

--- index.js
+++ index.js
@@ -1,5 +1,120 @@
 const csvOutput = document.getElementById("csv-data-output");
+const convertButton = document.getElementById("convert-json-button");
 
-function populateCSVData (csvData) {
-    csvOutput.innerHTML = csvData;
+/**
+ * 
+ * @param {String} jsonData
+ * @returns {Void}
+ */
+const convertJsonToCsv = jsonData => {
+    let parsedJSON;
+
+    try {
+        parsedJSON = JSON.parse(jsonData);
+    } catch {
+        alert(`Failed to parse JSON data. Please check the text you have entered is correctly formatted JSON.`);
+        csvOutput.innerHTML = null;
+    }
+
+    const csvData = createCsvData(parsedJSON);
+    let csvString = '';
+
+    for (const row of csvData) {
+        if (csvData.indexOf(row) !== csvData.length - 1) {
+            csvString += `${row}\n`;
+        }
+        else {
+            csvString += `${row}`;
+        }
+    }
+
+    csvOutput.innerHTML = csvString;
+};
+
+/**
+ * 
+ * @param {Object} jsonData 
+ * @returns {Array.<Array.<String>>}
+ */
+const createCsvData = jsonData => {
+    const keys = Object.keys(jsonData);
+    const columns = keys.map((key) => { return `"${key}"`});
+    const rows = getRows(jsonData);
+    let csvData = [columns];
+    
+    
+    const numRows = rows.length;
+    let i;
+
+    for (i = 0; i < numRows; i++) {
+        csvData.push(rows[i]);
+    }
+
+    return csvData;
+};
+
+/**
+ * 
+ * @param {Object} parsedJSON 
+ * @returns {Number}
+ */
+const getNumRows = parsedJSON => {
+    let numRows = 1;
+
+    for (const key in parsedJSON) {
+        if (Array.isArray(parsedJSON[key]) && parsedJSON[key].length > numRows) {
+            numRows = parsedJSON[key].length;
+        } 
+    }
+
+    return numRows;
 }
+
+/**
+ * 
+ * @param {Number} numRows 
+ * @param {Number} numColumns 
+ * @returns {Array}
+ */
+const initRows = (numRows, numColumns) => {
+    let rows = [];
+    let i, j;
+    
+    for (i = 0; i < numRows; i++) {
+        let row = [];
+        for (j = 0; j < numColumns; j++) {
+            row.push(null);
+        }
+        rows.push(row);
+    }
+
+    return rows;
+}
+
+/**
+ * @param {Object} parsedJSON 
+ * @returns {Array.<Array.<String>>}
+ */
+const getRows = parsedJSON => {
+    const columns = Object.keys(parsedJSON);
+    const numColumns = columns.length;
+    const numRows = getNumRows(parsedJSON);
+    let rows = initRows(numRows, numColumns);
+
+    for (const column of columns) {
+        if (Array.isArray(parsedJSON[column])) {
+            parsedJSON[column].forEach((element, index) => {
+                rows[index][columns.indexOf(column)] = element;
+            });
+        } else if (typeof parsedJSON[column] !== 'Object') {
+            rows[0][columns.indexOf(column)] = parsedJSON[column];
+        }
+    }
+
+    return rows;
+};
+
+convertButton.addEventListener("click", () => {
+    let jsonInput = document.getElementById("json-data-input");
+    convertJsonToCsv(jsonInput.value.toString());
+})
